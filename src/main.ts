5e4673fdd7264afd66dab80e2974d2fd65bdc53a
g0rs1n <gordeyttt011117@gmail.com>
2025-06-01 23:32:53 +0300

    setup environment and connect database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/allExceptions.filter';
 
 async function bootstrap() {
+  const {CORS_ORIGIN, PORT} = process.env
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    })
+  )
+  app.enableCors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+  })
+  app.setGlobalPrefix("/api")
+  app.useGlobalFilters(new AllExceptionsFilter())
+  try {
+    await app.listen(PORT ?? 3000)
+  } catch (error) {
+    console.error(error)
+  }
 }
 bootstrap();
