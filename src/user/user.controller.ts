f78770130b7946b3f3f41a0ad110f10463f86dc9
g0rs1n <gordeyttt011117@gmail.com>
2025-06-17 15:18:54 +0300

    Add LogOut UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post,
+  Res,
+  Req, 
+  UseGuards,
+  HttpCode
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { IUserBase } from 'src/common/utils/types';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 interface RequestWithUser extends Request {
   user: IUserBase;
@@ -19,4 +27,18 @@ export class UserController {
   ) {
     return req.user
   }
+
+  @HttpCode(200)
+  @Post('logout')
+  @UseGuards(AuthGuard('jwt'))
+  async logout(
+    @Res({passthrough: true}) res: Response
+  ) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production"
+    })
+    return {success: true, message: "Logged out successfully"}
+  }
+
 }
